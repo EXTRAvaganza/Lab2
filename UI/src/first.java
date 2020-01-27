7a43a9acb249147d4f54195d312b6c1c5cb2f0f6
EXTRAvaganza <37329819+EXTRAvaganza@users.noreply.github.com>
2020-01-28 01:26:44 +0400

    First
    controller test
    
    Co-Authored-By: extravaganza <extravaganza@users.noreply.github.com>

---
+++ UI/src/first.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class first {
+    public static void main(String[] args) throws IOException {
+        client kek = new client();
+    }
+}
---
+++ UI/src/third.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class third {
+
+        public static void main(String[] args) throws IOException {
+            client kek = new client();
+        }
+    }
+
